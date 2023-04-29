f0cf24f897286ed31f3216f2c9869fab4a81e1ae
PADMA B <padmab@PADMAs-MacBook-Pro.local>
2023-04-29 00:15:00 -0700

    removed extra new line to make test 1 pass

--- main.cpp
+++ main.cpp
@@ -88,7 +88,7 @@ int findD(int e, int phi){
         //cout << num << " ";
         multiplesofe.push_back(num);
     }
-    cout << endl;
+    //cout << endl;
 
     // for (int i = 0; i < multiplesofe.size(); ++i)
     // {
